8e909b489cfe3ba936edf1cd99faebaa8092cbdd
Tassilo Egerter <home@tassilo-egerter.de>
2025-02-16 22:36:50 +0100

    Enhance WiFi setup and display feedback in ESP32 Internet radio project

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,17 @@ Internetradio mit Nextion Display und ESP32.
 #include "NexButton.h"
 #include <WiFiManager.h>
 #include "Arduino.h"
-#include "AudioTools.h"   // AudioTools library coming soon
+#include "AudioTools.h" // AudioTools library coming soon
 #include "AudioTools/AudioLibs/VS1053Stream.h"
+#include "WiFi.h"
+#include <sstream>
+#include <string>
 
 // Display related stuff
 NexButton b_Schw = NexButton(0, 1, "bschw");
 NexButton b_bob = NexButton(0, 2, "bbob");
+NexText t0 = NexText(0, 3, "t0");
+NexText t1 = NexText(0, 4, "t1");
 NexTouch *nex_listen_list[] =
     {
         &b_Schw,
@@ -24,10 +29,9 @@ void Bob_PopCallback(void *ptr);
 void Bob_PushCallback(void *ptr);
 
 // Audio related stuff
-URLStream url("","");  // or replace with ICYStream to get metadata
-VS1053Stream vs1053; // final output
+URLStream url("", "");          // or replace with ICYStream to get metadata
+VS1053Stream vs1053;            // final output
 StreamCopy copier(vs1053, url); // copy url to decoder
-
 
 bool Setup_WiFi_();
 
@@ -36,13 +40,24 @@ void setup(void)
 
     setup_display();
     bool wifi = Setup_WiFi_();
+    if (wifi)
+    {
+       url.begin("http://stream.radioregenbogen.de/regenbogen-mp3-hq");
+       url.
+    }
+    else
+    {
+        dbSerialPrintln("WiFi not connected");
+    }
 
     pinMode(BUILTIN_LED, OUTPUT);
+
 }
 
 void loop(void)
 {
     nexLoop(nex_listen_list);
+
 }
 
 // Setup the display
@@ -92,10 +107,14 @@ bool Setup_WiFi_()
     {
         dbSerialPrintln("Failed to connect");
         ESP.restart();
+        return false; // to make the compiler happy
     }
     else
     {
+        std::ostringstream oss;
+        oss << "Connected! : " << WiFi.localIP().toString().c_str();
         dbSerialPrintln("connected...yeey :)");
+        t0.setText(oss.str().c_str());
         return true;
     }
 }
